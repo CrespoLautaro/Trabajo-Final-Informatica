2d4966bf656441ef37c553426d9755661316f77c
Juan Pablo Polvoreda <juan.polvoreda@alumnos.frm.utn.edu.ar>
2025-12-05 17:35:26 -0300

    Add files via upload

---
+++ Electrocardiografo_Arduino/Pulsador.cpp
@@ -0,0 +1,26 @@
+#include "Pulsador.h"
+
+// Constructor
+Pulsador::Pulsador(int p) {
+	pin = p;
+	ultimoEstado = HIGH; 
+}
+
+// Configuraci�n del pin
+void Pulsador::inicializar() {
+    pinMode(pin, INPUT_PULLUP); // <--- CAMBIO CLAVE
+    ultimoEstado = HIGH;        // Asumimos que arranca sin pulsar (HIGH por el pullup)
+}
+// L�gica del bot�n
+bool Pulsador::fuePresionado() {
+	int estadoActual = digitalRead(pin);
+	bool resultado = false;
+	
+	if (estadoActual == LOW && ultimoEstado == HIGH) {
+		resultado = true;
+		delay(50); // Anti-rebote
+	}
+	
+	ultimoEstado = estadoActual;
+	return resultado;
+}
---
+++ Electrocardiografo_Arduino/Pulsador.h
@@ -0,0 +1,19 @@
+#ifndef PULSADOR_H
+#define PULSADOR_H
+
+#include <Arduino.h>
+
+class Pulsador {
+private:
+	int pin;
+	int ultimoEstado;
+	
+public:
+	// Constructor
+	Pulsador(int p);
+	
+	void inicializar();
+	bool fuePresionado();
+};
+
+#endif
