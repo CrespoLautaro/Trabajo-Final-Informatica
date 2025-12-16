c216eb4a651554bd7305b739f1577be9e13e6a15
Juan Pablo Polvoreda <juan.polvoreda@alumnos.frm.utn.edu.ar>
2025-12-16 17:15:01 -0300

    Update Pulsador.cpp
    Cambio delay por millis

--- Electrocardiografo_Arduino/Pulsador.cpp
+++ Electrocardiografo_Arduino/Pulsador.cpp
@@ -1,5 +1,6 @@
 #include "Pulsador.h"
-
+unsigned long ultimoTiempo;
+  const unsigned long debounce = 30;
 // Constructor
 Pulsador::Pulsador(int p) {     //de la forma, Clase::constructor
 	pin = p;
@@ -15,16 +16,17 @@ void Pulsador::inicializar() {
 bool Pulsador::fuePresionado() {
 	int estadoActual = digitalRead(pin);
 	bool resultado = false;
-	
+	unsigned long tiempoAhora = millis();
+
 	if (estadoActual == LOW && ultimoEstado == HIGH) {
-		resultado = true;
-		delay(50); // Anti-rebote
-	}
+		// Solo aceptamos la pulsación si pasaron más de 50ms desde la última
+     if (tiempoAhora - ultimoTiempo > retardoantirrebote) {
+         resultado = true;
+         ultimoTiempo = tiempoAhora; // Actualizamos el cronómetro
+        }
+		}
 	
 	ultimoEstado = estadoActual;
 	return resultado;
 
 }
-
-
-
